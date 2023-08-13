cc9c18ba617cdcf0a1939d346f23bdf97063cb91
Roxy777 <142011807+RoxyXploit777@users.noreply.github.com>
2023-08-13 13:38:09 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'REYYX'// yourname
+global.MONGO_DB_URI = "mongodb+srv://REYYX:<password>@cluster0.d9dozmo.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "666xcoder@gmail.com" //email
+global.email_password = "dtiqkemdcwswdneu" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
